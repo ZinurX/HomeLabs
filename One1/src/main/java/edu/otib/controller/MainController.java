0fa919c62cab033b1aa82010727a82024896f2fc
ZinurX <145262061+ZinurX@users.noreply.github.com>
2024-12-05 09:33:55 +0300

    Add new web file;

--- One1/src/main/java/edu/otib/controller/MainController.java
+++ One1/src/main/java/edu/otib/controller/MainController.java
@@ -1,7 +1,15 @@
 package edu.otib.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping (value="/model")
 public class MainController {
+
+    @GetMapping
+    public String autos(){
+
+    }
 }

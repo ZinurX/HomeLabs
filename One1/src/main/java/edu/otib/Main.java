0ddb9d6bc6210157c0bb03c5d8b021a43982c612
ZinurX <145262061+ZinurX@users.noreply.github.com>
2024-12-05 09:24:36 +0300

    Add new inc count;

--- One1/src/main/java/edu/otib/Main.java
+++ One1/src/main/java/edu/otib/Main.java
@@ -5,12 +5,10 @@ package edu.otib;
 public class Main {
     public static void main(String[] args) {
         String name="friend";
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            int count=0;
+            System.out.println("Hi " + name + "i = " + i + "count = " + count);
         }
     }
 }
